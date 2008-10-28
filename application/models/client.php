bb8c8e70847a85a68f0d642e003ef779f052eef6
zombor <>
2008-10-28 02:00:15 +0000

    Doing some overloading in the client model for special access.

--- application/models/client.php
+++ application/models/client.php
@@ -23,7 +23,26 @@ class Client_Model extends Auto_Modeler_ORM
 	                         'phone_number' => array('phone'),
 	                         'mailing_zip_code' => array('numeric'));
 
-	// Overriding the save method to set the short_name (url::title() of the company_name)
+	// Overloading constructor to load by a second column
+	public function __construct($id = NULL)
+	{
+		parent::__construct();
+
+		if ($id != NULL)
+		{
+			// try and get a row with this ID
+			$data = $this->db->from($this->table_name)->orwhere(array('id' => $id, 'short_name' => $id))->result(FALSE);
+
+			// try and assign the data
+			if (count($data) == 1 AND $data = $data->current())
+			{
+				foreach ($data as $key => $value)
+					$this->data[$key] = $value;
+			}
+		}
+	}
+
+	// Overloading the save method to set the short_name (url::title() of the company_name)
 	public function save($extra_data = array(), $extra_validation_methods = array())
 	{
 		$this->data['short_name'] = url::title($this->data['company_name']);
