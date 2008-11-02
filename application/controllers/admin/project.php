2be2771b5b3b291aaca277fc0a27106089b3876f
zombor <>
2008-11-02 05:45:46 +0000

    Fixing #5.

--- application/controllers/admin/project.php
+++ application/controllers/admin/project.php
@@ -11,13 +11,21 @@ class Project_Controller extends Website_Controller {
 			$this->template->body->errors = '';
 			$this->template->body->project = $project;
 			$this->template->body->title = 'Add';
+			$this->template->body->client = new Client_Model();
 		}
 		else
 		{
-			$project->set_fields($this->input->post());
+			$project->set_fields($this->input->post('project'));
 
 			try
 			{
+				if ($_POST['project']['client_id'] == 'new')
+				{
+					$client = new Client_Model();
+					$client->set_fields($this->input->post('client'));
+					$client->save();
+				}
+
 				$project->save();
 				url::redirect('project/view/'.$project->id);
 			}
@@ -27,6 +35,7 @@ class Project_Controller extends Website_Controller {
 				$this->template->body->project = $project;
 				$this->template->body->errors = $e;
 				$this->template->body->title = 'Add';
+				$this->template->body->client = $client;
 			}
 		}
 	}
@@ -40,6 +49,7 @@ class Project_Controller extends Website_Controller {
 			$this->template->body->errors = '';
 			$this->template->body->project = $project;
 			$this->template->body->title = 'Update';
+			$this->template->body->client = new Client_Model();
 		}
 		else
 		{
@@ -47,6 +57,13 @@ class Project_Controller extends Website_Controller {
 
 			try
 			{
+				if ($_POST['project']['client_id'] == 'new')
+				{
+					$client = new Client_Model();
+					$client->set_fields($this->input->post('client'));
+					$client->save();
+				}
+
 				$project->save();
 				url::redirect('project/view/'.$project->id);
 			}
@@ -56,6 +73,7 @@ class Project_Controller extends Website_Controller {
 				$this->template->body->project = $project;
 				$this->template->body->errors = $e;
 				$this->template->body->title = 'Update';
+				$this->template->body->client = $client;
 			}
 		}
 	}
--- application/views/admin/project/form.php
+++ application/views/admin/project/form.php
@@ -2,9 +2,24 @@
 <?=$errors?>
 <?=form::open()?>
 <ul>
-	<li><label for="name">Project Name:</label> <?=form::input('name', $project->name)?></li>
-	<li><label for="client_id">Assigned Client:</label> <?=form::dropdown('client_id', Auto_Modeler_ORM::factory('client')->select_list('id', 'company_name'), $project->client_id)?></li>
-	<li><label for="notes">Project Notes:</label><br /><?=form::textarea(array('name' => 'notes', 'value' => $project->notes))?></li>
+	<li><label for="name">Project Name:</label> <?=form::input('project[name]', $project->name)?></li>
+	<li><label for="client_id">Assigned Client:</label> <?=form::dropdown('project[client_id]', Auto_Modeler_ORM::factory('client')->select_list('id', 'company_name') + array('new' => 'New Client...'), $project->client_id)?></li>
+	<li><label for="notes">Project Notes:</label><br /><?=form::textarea(array('name' => 'project[notes]', 'value' => $project->notes))?></li>
 	<li><?=form::submit('submit', $title.' Project')?></li>
 </ul>
+<div id="new_client">
+<h3>New Client Information</h3>
+<ul>
+	<li><label for="company_name">Company Name:</label> <?=form::input('client[company_name]', $client->company_name)?></li>
+	<li><label for="contact_first_name">Contact First Name:</label> <?=form::input('client[contact_first_name]', $client->contact_first_name)?></li>
+	<li><label for="contact_first_name">Contact Last Name:</label> <?=form::input('client[contact_last_name]', $client->contact_last_name)?></li>
+	<li><label for="mailing_address">Mailing Address:</label><br /><?=form::textarea(array('name' => 'client[mailing_address]', 'value' => $client->mailing_address))?></li>
+	<li><label for="mailing_city">Mailing City:</label> <?=form::input('client[mailing_city]', $client->mailing_city)?></li>
+	<li><label for="mailing_state">Mailing State:</label> <?=form::input('client[mailing_state]', $client->mailing_state)?></li>
+	<li><label for="mailing_zip_code">Mailing Zip Code:</label> <?=form::input('client[mailing_zip_code]', $client->mailing_zip_code)?></li>
+	<li><label for="email_address">Email Address:</label> <?=form::input('client[email_address]', $client->email_address)?></li>
+	<li><label for="phone_number">Phone Number:</label> <?=form::input('client[phone_number]', $client->phone_number)?></li>
+	<li><label for="tax_exempt">Tax Expemt:</label> <?=form::checkbox('client[tax_exempt]', TRUE, $client->tax_exempt)?></li>
+</ul>
+</div>
 <?form::close()?>
