f6ce6dfc0d7713be731f6eb83ee4fca2bab44998
yinkei <>
2008-11-06 16:00:53 +0000

    Reverted r74 and moved content-type tag to top of head

--- application/views/template.php
+++ application/views/template.php
@@ -2,6 +2,7 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
 	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 		<meta http-equiv="content-language" content="en" />
 		<title><?=$title?></title>
 		<?=html::stylesheet(array('css/reset', 'css/style', 'css/print'), array('', '', 'print'))?>
