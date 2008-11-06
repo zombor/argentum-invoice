8d323e642b6befc8415df0df526bc65d2def906e
yinkei <>
2008-11-06 08:54:14 +0000

    lower cased Content-type

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
 	<head>
 		<meta http-equiv="content-language" content="en" />
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 		<title><?=$title?></title>
 		<?=html::stylesheet(array('css/reset', 'css/style', 'css/print'), array('', '', 'print'))?>
 
