11f1e5a7b735084d3e47bd70e5dd0a79bcc84fc3
yinkei <>
2008-11-06 09:27:32 +0000

    Removed clear class from header container and specify a height instead (because atm, height is fixed)

--- application/views/template.php
+++ application/views/template.php
@@ -13,7 +13,7 @@
 	</head>
 	<body>
 		<div id="wrapper">
-			<div id="header" class="clear">
+			<div id="header">
 				<?=html::image(array('src' => 'images/argentum_logo_tagline.png', 'alt' => 'Argentum', 'class' => 'clear'))?>
 				<ul id="nav">
 					<li><?=html::anchor('', 'Home')?></li>
