c479d2361b5f85a09547399e5d7142564f89b72d
zombor <>
2008-11-13 04:22:07 +0000

    Implementing the project part of #49.

--- application/controllers/admin/project.php
+++ application/controllers/admin/project.php
@@ -39,7 +39,21 @@ class Project_Controller extends Website_Controller {
 					$client = $project->client;
 
 				$project->save();
-				url::redirect('project/view/'.$project->id);
+
+				// Send an email to all users who are set to receive emails on new projects
+				$swift = email::connect();
+				$message = new Swift_Message('New Project Created- ID:'.$project->id.', '.$project->title, View::factory('emails/project_creation')->set(array('project' => $project)));
+				$recipients = new Swift_RecipientList();
+
+				foreach (Auto_Modeler_ORM::factory('user')->fetch_some(array('email_project_create' => TRUE)) as $user)
+				{
+					$recipients->addTo($user->email, $user->first_name.' '.$user->last_name);
+				}
+
+				if ($swift->send($message, $recipients, $_SESSION['auth_user']->email))
+					url::redirect('project/view/'.$project->id);
+				else
+					throw new Kohana_User_Exception('swift.general_error');
 			}
 			catch (Kohana_User_Exception $e)
 			{
@@ -85,7 +99,26 @@ class Project_Controller extends Website_Controller {
 					$client = $project->client;
 
 				$project->save();
-				url::redirect('project/view/'.$project->id);
+
+				if ($project->complete)
+				{
+					// Send an email to all users who are set to receive emails on new projects
+					$swift = email::connect();
+					$message = new Swift_Message('New Project Created- ID:'.$project->id.', '.$project->title, View::factory('emails/project_creation')->set(array('project' => $project)));
+					$recipients = new Swift_RecipientList();
+
+					foreach (Auto_Modeler_ORM::factory('user')->fetch_some(array('email_project_create' => TRUE)) as $user)
+					{
+						$recipients->addTo($user->email, $user->first_name.' '.$user->last_name);
+					}
+
+					if ($swift->send($message, $recipients, $_SESSION['auth_user']->email))
+						url::redirect('project/view/'.$project->id);
+					else
+						throw new Kohana_User_Exception('swift.general_error');
+				}
+				else
+					url::redirect('project/view/'.$project->id);
 			}
 			catch (Kohana_User_Exception $e)
 			{
