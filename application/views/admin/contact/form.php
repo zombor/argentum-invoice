4bd6a0e7b5c4c2ffedbf901374c1f5df79122e5b
zombor <>
2009-05-04 12:15:22 +0000

    Fixing #101.

--- application/views/admin/contact/form.php
+++ application/views/admin/contact/form.php
@@ -6,8 +6,8 @@
 		<li><label for="first_name">First Name:</label> <?=form::input('first_name', $contact->first_name)?></li>
 		<li><label for="last_name">Last Name:</label> <?=form::input('last_name', $contact->last_name)?></li>
 		<li><label for="email">Email Address:</label> <?=form::input('email', $contact->email)?></li>
-		<li><label for="project_client_id">Assigned Clients:</label><br />
-			<?php foreach ($clients as $client):?><?=form::checkbox('client['.$client->id.']', $client->id, $contact->belongs_to('clients', $client->id))?> <?=$client->company_name?><br />
+		<li><label for="project_client_id">Assigned Clients:</label><br /><br /><br />
+			<?php foreach ($clients as $client):?><?=form::checkbox('client['.$client->id.']', $client->id, $contact->has('clients', $client->id))?> <?=$client->company_name?><br />
 			<?php endforeach; ?>
 		<li><?=form::submit('submit', $title.' Contact')?></li>
 	</ul>
