470f7b03a0b56f39f2e60591b4410025c6e90145
zombor <>
2009-08-01 15:21:50 +0000

    Fixing #140.

--- application/views/ticket/active.php
+++ application/views/ticket/active.php
@@ -18,7 +18,7 @@
 			<td><?=Markdown($ticket->description)?></td>
 			<td><?=$ticket->operation_type->name?></td>
 			<td><?=number_format($ticket->total_time, 2)?> Hours</td>
-			<td><?=html::anchor('admin/time/add/'.$ticket->id, html::image(array('src' => 'images/icons/time_add.png', 'alt' => 'Add Time')), array('class' => 'add_time'))?> <?=html::anchor('admin/ticket/edit/'.$ticket->id, html::image(array('src' => 'images/icons/pencil.png', 'alt' => 'Edit Ticket')), array('class' => 'edit_ticket'))?> <?=form::open('admin/ticket/delete', NULL, array('id' => $ticket->id))?><?=form::input(array('src' => url::base().'images/icons/cross.png', 'alt' => 'Delete Ticket', 'type' => 'image'))?><?=form::close()?>
+			<td><?=html::anchor('admin/time/add/'.$ticket->id, html::image(array('src' => 'images/icons/time_add.png', 'alt' => 'Add Time')), array('class' => 'add_time'))?> <?=html::anchor('admin/ticket/edit/'.$ticket->id, html::image(array('src' => 'images/icons/pencil.png', 'alt' => 'Edit Ticket')), array('class' => 'edit_ticket'))?> <?=form::open('admin/ticket/delete', array('class' => 'form_delete'), array('id' => $ticket->id))?><?=form::input(array('src' => url::base().'images/icons/cross.png', 'alt' => 'Delete Ticket', 'type' => 'image'))?><?=form::close()?>
 			    <?php Event::run('argentum.active_ticket_item_display', $ticket)?></td>
 	</tr><?php endforeach;?>
 	</tbody>
