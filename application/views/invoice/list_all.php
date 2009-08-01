b712296cbabed69f7030f57dcb08f7292c9bcd2a
zombor <>
2009-08-01 16:22:31 +0000

    Fixing #108.

--- application/views/invoice/list_all.php
+++ application/views/invoice/list_all.php
@@ -1,6 +1,8 @@
 <?php
 	$total_income = 0;
 	$total_paid = 0;
+	$total_subtotal = 0;
+	$total_tax = 0;
 ?>
 <h1>View All Invoices</h1>
 <p><?php for ($i = date('Y'); $i >= 2008; $i--): ?>
@@ -14,18 +16,31 @@
 		<tr>
 			<th>Invoice ID</th>
 			<th>Client</th>
+			<th>Subtotal</th>
+			<th>Tax</th>
 			<th>Total Income</th>
 			<th>Total Paid</th><?php if (Auth::instance()->logged_in('admin')):?>
 			<td>Admin</td><?php endif;?>
 		</tr>
 		<?php foreach ($invoices as $invoice):?><tr<?php if ($invoice->total_income() > $invoice->total_paid()):?> class="unpaid"<?php endif;?>>
+			<?php
+				$invoice_income = $invoice->total_income();
+				$invoice_paid = $invoice->total_paid();
+				$invoice_subtotal = $invoice->subtotal();
+				$invoice_tax = $invoice->find_sales_tax();
+				$total_income+=$invoice_income;
+				$total_paid+=$invoice_paid;
+				$total_subtotal+=$invoice_subtotal;
+				$total_tax+=$invoice_tax;
+			?>
 			<td><?=html::anchor('invoice/view/'.$invoice->id, $invoice->id)?></td>
 			<td><?=$invoice->client->company_name?></td>
-			<td>$<?=number_format($invoice->total_income(), 2)?></td>
-			<td>$<?=number_format($invoice->total_paid(), 2)?></td>
+			<td>$<?=number_format($invoice_subtotal, 2)?></td>
+			<td>$<?=number_format($invoice_tax, 2)?></td>
+			<td>$<?=number_format($invoice_income, 2)?></td>
+			<td>$<?=number_format($invoice_paid, 2)?></td>
 			<?php
-				$total_income+=$invoice->total_income();
-				$total_paid+=$invoice->total_paid();
+				
 			?>
 			<?php if (Auth::instance()->logged_in('admin')):?><td>
 				<?=html::anchor('admin/invoice/post_payment/'.$invoice->id, html::image(array('src' => 'images/icons/money_add.png', 'alt' => 'Post Payment')))?>
@@ -35,6 +50,8 @@
 		<?php endforeach;?></tr>
 		<tr class="total_row">
 			<td colspan="2"></td>
+			<td>$<?=number_format($invoice_subtotal, 2)?></td>
+			<td>$<?=number_format($invoice_tax, 2)?></td>
 			<td>$<?=number_format($total_income, 2)?></td>
 			<td>$<?=number_format($total_paid, 2)?></td>
 			<?php if (Auth::instance()->logged_in('admin')):?><td></td><?php endif;?>
