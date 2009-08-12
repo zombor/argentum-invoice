bbdde89caa47dcc73aa6a67265fa33527cafa8dd
zombor <>
2009-08-12 17:15:52 +0000

    Fixing #166.

--- application/views/project/_list.php
+++ application/views/project/_list.php
@@ -8,10 +8,10 @@
 <ul id="project_list">
 	<?php foreach ($projects as $project):?> 
 	<li><a href="<?=url::site('project/view/'.$project->id);?>">
+		<div class="task_count">ACTIVE TICKETS: <strong><?=count($project->find_related('tickets', array('complete' => 0)));?></strong></div>
 		<div class="project_id">ID: <?=$project->id?></div>
 		<div class="client"><?=$project->client->company_name?></div>
 		<div class="name"><?=$project->name?></div>
-		<div class="task_count">ACTIVE TICKETS: <strong><?=count($project->find_related('tickets', array('complete' => 0)));?></strong></div>
 	</a></li>
 	<?php endforeach?>
 </ul>
