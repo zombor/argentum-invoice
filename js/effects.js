67bc16d0ae2f990baa700678c52c5eb38343cb66
Jeremy Bush <contractfrombelow@gmail.com>
2009-12-01 21:46:55 -0600

    Fixing #190.

--- js/effects.js
+++ js/effects.js
@@ -21,4 +21,80 @@ $(document).ready(function() {
 	});
 
 	$("table.invoice_list").tablesorter();
+
+	$('table#invoice_form input[type=checkbox]').click(function()
+	{
+		var total_cost = parseFloat($('span.total_cost').html());
+		var this_cost = parseFloat($(this).attr('rel'));
+		if ($(this).is(':checked'))
+		{
+			var total = total_cost+this_cost;
+			$('span.total_cost').html(number_format(total, 2));
+		}
+		else
+		{
+			$('span.total_cost').html(number_format(total_cost-this_cost, 2));
+		}
+	})
 });
+
+function number_format (number, decimals, dec_point, thousands_sep)
+{
+	// Formats a number with grouped thousands
+	//
+	// version: 906.1806
+	// discuss at: http://phpjs.org/functions/number_format
+	// +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	// +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// +	 bugfix by: Michael White (http://getsprink.com)
+	// +	 bugfix by: Benjamin Lupton
+	// +	 bugfix by: Allan Jensen (http://www.winternet.no)
+	// +	revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	// +	 bugfix by: Howard Yeend
+	// +	revised by: Luke Smith (http://lucassmith.name)
+	// +	 bugfix by: Diogo Resende
+	// +	 bugfix by: Rival
+	// +	 input by: Kheang Hok Chin (http://www.distantia.ca/)
+	// +	 improved by: davook
+	// +	 improved by: Brett Zamir (http://brett-zamir.me)
+	// +	 input by: Jay Klehr
+	// +	 improved by: Brett Zamir (http://brett-zamir.me)
+	// +	 input by: Amir Habibi (http://www.residence-mixte.com/)
+	// +	 bugfix by: Brett Zamir (http://brett-zamir.me)
+	var n = number, prec = decimals;
+
+	var toFixedFix = function (n,prec) {
+		var k = Math.pow(10,prec);
+		return (Math.round(n*k)/k).toString();
+	};
+
+	n = !isFinite(+n) ? 0 : +n;
+	prec = !isFinite(+prec) ? 0 : Math.abs(prec);
+	var sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep;
+	var dec = (typeof dec_point === 'undefined') ? '.' : dec_point;
+
+	var s = (prec > 0) ? toFixedFix(n, prec) : toFixedFix(Math.round(n), prec); //fix for IE parseFloat(0.55).toFixed(0) = 0;
+
+	var abs = toFixedFix(Math.abs(n), prec);
+	var _, i;
+
+	if (abs >= 1000) {
+		_ = abs.split(/\D/);
+		i = _[0].length % 3 || 3;
+
+		_[0] = s.slice(0,i + (n < 0)) +
+			  _[0].slice(i).replace(/(\d{3})/g, sep+'$1');
+		s = _.join(dec);
+	} else {
+		s = s.replace('.', dec);
+	}
+
+	var decPos = s.indexOf(dec);
+	if (prec >= 1 && decPos !== -1 && (s.length-decPos-1) < prec) {
+		s += new Array(prec-(s.length-decPos-1)).join(0)+'0';
+	}
+	else if (prec >= 1 && decPos === -1) {
+		s += dec+new Array(prec).join(0)+'0';
+	}
+	return s;
+}
--- modules/argentum_core/views/admin/invoice/create.php
+++ modules/argentum_core/views/admin/invoice/create.php
@@ -11,6 +11,8 @@
  * @property array  $template array of available invoice templates
  * @property array  $projects array of project tickets and non-hourly items 
  */
+
+ $total_cost = 0;
 ?>
 <h2>Create Invoice For <?=$client->company_name?></h2>
 <?=form::open('admin/invoice/create')?>
@@ -23,7 +25,7 @@
 <p><?=form::dropdown('template_name', $templates)?></p>
 <h3>Tickets to be billed:</h3>
 <table id="invoice_form">
-	<tbody>
+	<thead>
 		<tr>
 			<th>Bill</th>
 			<th>Ticket ID</th>
@@ -33,17 +35,19 @@
 			<th>Rate</th>
 			<th>Ticket Total Cost</th>
 		</tr>
+	</thead>
+	<tbody>
 		<?php foreach ($projects['tickets'] as $project_id => $tickets):?><tr>
 			<th colspan="7" class="project">Project: <?=Auto_Modeler_ORM::factory('project', $project_id)->name?></th>
 		</tr>
 		<?php foreach ($tickets as $ticket):?><tr>
-			<td><?=form::checkbox('tickets['.$ticket->id.']', $ticket->id, TRUE)?></td>
+			<td><?=form::checkbox(array('name' => 'tickets['.$ticket->id.']', 'value' => $ticket->id, 'checked' => TRUE, 'rel' => $ticket->total_time*$ticket->rate))?></td>
 			<td><?=$ticket->id?></td>
 			<td><?=$ticket->user == NULL ? 'Unassigned' : $ticket->user->username ?></td>
 			<td><?=$ticket->description?></td>
 			<td><?=number_format($ticket->total_time, 2)?></td>
 			<td>$<?=number_format($ticket->rate, 2)?></td>
-			<td>$<?=number_format($ticket->total_time*$ticket->rate, 2)?></td>
+			<td>$<?=number_format($ticket->total_time*$ticket->rate, 2)?><?php $total_cost+=$ticket->total_time*$ticket->rate?></td>
 		</tr><?php endforeach;?>
 		<?php endforeach;?>
 		<tr>
@@ -60,14 +64,23 @@
 			<th colspan="7" class="project">Project: <?=Auto_Modeler_ORM::factory('project', $project_id)->name?></th>
 		</tr>
 		<?php foreach ($non_hourlies as $non_hourly):?><tr>
-			<td><?=form::checkbox('non_hourly['.$non_hourly->id.']', $non_hourly->id, TRUE)?></td>
+			<td><?=form::checkbox(array('name' => 'non_hourly['.$non_hourly->id.']', 'value' => $non_hourly->id, 'checked' => TRUE, 'rel' => $non_hourly->cost))?></td>
 			<td><?=$non_hourly->id?></td>
 			<td colspan="3"><?=$non_hourly->description?></td>
 			<td><?=$non_hourly->quantity?></td>
-			<td>$<?=number_format($non_hourly->cost, 2)?></td>
+			<td>$<?=number_format($non_hourly->cost, 2)?><?php $total_cost+=$non_hourly->cost?></td>
 		</tr><?php endforeach;?>
 		<?php endforeach;?>
 	</tbody>
+	<tfoot>
+		<tr>
+			<td colspan="7" class="project"><h3>Totals</h3></td>
+		</tr>
+		<tr>
+			<td colspan="6"></td>
+			<td><strong>$<span class="total_cost"><?=number_format($total_cost, 2)?></span></strong></td>
+		</tr>
+	</tfoot>
 </table>
 <p><input type="submit" value="Create Invoice" <?php if ( ! count($projects['tickets']) AND ! count($projects['non_hourly'])):?>disabled="disabled" <?php endif;?>/></p>
 <?=form::close()?>
